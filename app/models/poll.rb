a0fe48c302426a3e421136ef465d49e0fe37438c
Garrett Simpson <elgaricimo@gmail.com>
2015-06-14 12:14:17 -0700

    Fix typeo in model validations

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,6 +2,6 @@ class Poll < ActiveRecord::Base
   belongs_to :battle
   has_many :poll_votes
 
-  validates :name, presence: true, uniquness: true
+  validates :name, presence: true, uniqueness: true
   validates :user_id, :battle_id, presence: true
 end
--- app/models/poll_vote.rb
+++ app/models/poll_vote.rb
@@ -2,5 +2,5 @@ class PollVote < ActiveRecord::Base
   belongs_to :poll
 
   validates :user_id, :vine_vote_id, :poll_id, :save_poll, presence: true
-  validates_uniquness_of :user_id, scope: :poll_id
+  validates_uniqueness_of :user_id, scope: :poll_id
 end
