364065224f28ce42cc3eb86c3de8e268413a6411
Garrett Simpson <elgaricimo@gmail.com>
2015-06-16 00:44:01 -0700

    Put vine id in jbuilder, eagerload polls of battle

--- app/controllers/api/battles_controller.rb
+++ app/controllers/api/battles_controller.rb
@@ -6,8 +6,10 @@ class Api::BattlesController < ApplicationController
 
   def index
     @battles = Battle.includes(
-      {challenger_vine: :vine_author}, {acceptor_vine: :vine_author}
-    ).all
+      {challenger_vine: :vine_author},
+      {acceptor_vine: :vine_author},
+      {polls: :poll_votes}
+      ).all
     render :index
   end
 
