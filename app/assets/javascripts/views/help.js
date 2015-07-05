a666b988637dbd3a447279518c111a88624ba1c0
Garrett Simpson <elgaricimo@gmail.com>
2015-07-04 23:42:26 -0700

    finish getting Shepherd setup

--- app/assets/javascripts/views/help.js
+++ app/assets/javascripts/views/help.js
@@ -13,13 +13,17 @@ Vensei.Views.HelpIcon = Backbone.CompositeView.extend({
 	addTour: function(){
 		Vensei.tour = new Shepherd.Tour({
 			defaults: {
-				classes: 'shepherd-theme-arrows',
+				classes: 'shepherd-theme-arrows'
 			}
 		});
 
 		Vensei.tour.addStep('welcomeStep', {
 			title: 'Welcome to Vensei!',
-			text: "Vensei is a service which allows users to pin visual bookmarks to a virtual pinboard and share with friends. If you\'ve ever used Pinterest, this should be very familiar!",
+			text: "Vensei lets you make live-updating polls that" +
+			" compare the comedic value of two Vines. As your audience"+
+			" submits their responses over the web or sms, polls"+
+			" update live. If you\'ve ever used Poll"+
+			" Everywhere, this should be very familiar!",
 			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
 			buttons: [
 				{
@@ -37,9 +41,9 @@ Vensei.Views.HelpIcon = Backbone.CompositeView.extend({
 		});
 
 		Vensei.tour.addStep('cardStep', {
-			title: 'Pins: The Heart of Vensei',
-			text: "Each Pin is a Bookmark featuring an image, a link, a description.",
-			attachTo: '.card',
+			title: 'Live demo',
+			text: "This is a live demo; play the vids and vote!",
+			attachTo: '.computer',
 			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
 			buttons: [
 				{
@@ -57,9 +61,10 @@ Vensei.Views.HelpIcon = Backbone.CompositeView.extend({
 		});
 
 		Vensei.tour.addStep('cardStep', {
-			title: 'Hovering',
-			text: "By hovering over the pin you can \"repin\" it to one of your own boards or \"heart\" (like) it!",
-			attachTo: '.card',
+			title: 'Live Updates',
+			text: "Like all Vensei Polls, this poll updates live when"+
+			" anyone votes; have a friend vote and see it update!",
+			attachTo: '.computer',
 			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
 			buttons: [
 				{
@@ -77,9 +82,9 @@ Vensei.Views.HelpIcon = Backbone.CompositeView.extend({
 		});
 
 		Vensei.tour.addStep('cardStep', {
-			title: 'Author Info',
-			text: "Clicking the lower portion of the pin leads to Board this pin is sourced from.  Clicking the Avatar will take you to the author's profile!",
-			attachTo: '.pinner',
+			title: 'Creating polls',
+			text: "Create polls by entering in the urls of two Vines.",
+			attachTo: '.new-poll',
 			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
 			buttons: [
 				{
@@ -97,9 +102,10 @@ Vensei.Views.HelpIcon = Backbone.CompositeView.extend({
 		});
 
 		Vensei.tour.addStep('cardStep', {
-			title: 'Biggify Things!',
-			text: "Clicking on the image will enlarge the pin, provide more details, and let you read and leave comments.",
-			attachTo: '.image-area',
+			title: 'Browsing Polls',
+			text: "Make sure your headphones are on! Clicking this button will play "+
+			"Vines for you to browse and compare.",
+			attachTo: '.browse-battles',
 			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
 			buttons: [
 				{
@@ -117,89 +123,13 @@ Vensei.Views.HelpIcon = Backbone.CompositeView.extend({
 		});
 
 		Vensei.tour.addStep('cardStep', {
-			title: 'Infinite Feed',
-			text: "Your feed is populated with pins from the various boards you \"follow\".  More content is dynamically added as you scroll down!",
+			title: 'Get points for knowing what people think are funny',
+			text: "As you vote on browsed vines, if your choice matches the Vine"+
+			" that most people thought was funnier, you'll get points. Otherwise,"+
+			" you'll lose points. Score is displayed in the navbar up top.",
+			scrollTo: true,
+			attachTo: '.response-block',
 			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-			buttons: [
-				{
-					text: 'Next',
-					action: Vensei.tour.next,
-					classes: 'shepherd-button-example-primary'
-				},
-
-				{
-					text: 'End Tutorial',
-					classes: 'shepherd-button-secondary',
-					action: Vensei.tour.complete
-				}
-			]
-		});
-
-		Vensei.tour.addStep('cardStep', {
-			title: 'New Pins and Boards',
-			text: "Adding New Content is easy! Click on on the \"plus\" button and follow the instructions!",
-			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-			attachTo: '.plus top',
-			buttons: [
-				{
-					text: 'Next',
-					action: Vensei.tour.next,
-					classes: 'shepherd-button-example-primary'
-				},
-
-				{
-					text: 'End Tutorial',
-					classes: 'shepherd-button-secondary',
-					action: Vensei.tour.complete
-				}
-			]
-		});
-
-		Vensei.tour.addStep('cardStep', {
-			title: 'Your Profile',
-			text: "View your Boards and Profile by Clicking on your name. Currently, you are logged in as " + CURRENT_USER_USERNAME,
-			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-			attachTo: '.current-user bottom',
-			buttons: [
-				{
-					text: 'Next',
-					action: Vensei.tour.next,
-					classes: 'shepherd-button-example-primary'
-				},
-
-				{
-					text: 'End Tutorial',
-					classes: 'shepherd-button-secondary',
-					action: Vensei.tour.complete
-				}
-			]
-		});
-
-		Vensei.tour.addStep('cardStep', {
-			title: 'Notifications',
-			text: "View your Notifications by Clicking on the red Talk Icon.",
-			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-			attachTo: '.current-user bottom',
-			buttons: [
-				{
-					text: 'Next',
-					action: Vensei.tour.next,
-					classes: 'shepherd-button-example-primary'
-				},
-
-				{
-					text: 'End Tutorial',
-					classes: 'shepherd-button-secondary',
-					action: Vensei.tour.complete
-				}
-			]
-		});
-
-		Vensei.tour.addStep('cardStep', {
-			title: 'Search',
-			text: "The Search Bar will search the description portion of all pins for matching content.  Try Searching for the word \"Zack\"",
-			classes: 'shepherd shepherd-open shepherd-theme-arrows shepherd-transparent-text',
-			attachTo: '.search bottom',
 			buttons: [
 				{
 					text: 'Next',
