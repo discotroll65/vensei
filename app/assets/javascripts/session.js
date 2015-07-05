ae8d25db36de1dd590a818b8fa8f5b350dd7e2eb
Garrett Simpson <elgaricimo@gmail.com>
2015-07-04 18:58:04 -0700

    Make guest tour log you in as Goku

---
+++ app/assets/javascripts/session.js
@@ -0,0 +1,33 @@
+window.Guest = {
+	initialize: function(){
+		$(".guest-login").on( "click", this.guestLogin.bind(this) );
+	},
+
+	guestLogin: function(event){
+    event.preventDefault();
+		var that = this;
+		$username = $('#username');
+		$password = $('#password');
+		$submitButton = $('.login-button');
+
+		this.slowtype($username, 'Goku', function(){
+			that.slowtype($password, 'password', function(){
+				$submitButton.click();
+			})
+		});
+	},
+
+	slowtype: function($el, word, callback){
+
+		var typing = setInterval(function(){
+			$el.val( $el.val() + word.slice(0,1) );
+			word = word.substr(1);
+
+			if (!word){
+				clearInterval(typing);
+				callback();
+			}
+		}, 40);
+	}
+
+};
